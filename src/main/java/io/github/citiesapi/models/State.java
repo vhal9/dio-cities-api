fea7479d40e7cd0e29be20c98482fe1e881c3874
Victor Hugo Landin <victor.h.landin@gmail.com>
2021-08-13 21:03:27 -0300

    adiciona mapeamento do atributo DDD da classe State

--- src/main/java/io/github/citiesapi/models/State.java
+++ src/main/java/io/github/citiesapi/models/State.java
@@ -1,6 +1,10 @@
 package io.github.citiesapi.models;
 
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import lombok.Data;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
 
 import javax.persistence.*;
 import java.util.List;
@@ -8,6 +12,9 @@ import java.util.List;
 @Entity
 @Table(name = "estado")
 @Data
+@TypeDefs({
+        @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
+})
 public class State {
 
     @Id
@@ -25,6 +32,8 @@ public class State {
     @JoinColumn(name = "pais", referencedColumnName = "id")
     private Country country;
 
+    @Type(type = "jsonb")
+    @Basic(fetch = FetchType.LAZY)
     @Column(name = "ddd", columnDefinition = "jsonb")
     private List<Integer> ddd;
 
